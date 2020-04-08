681d83da170dadc9c6ec708372b729e16ac57637
Alex Arvanitidis <alex.arvanitidis@gmail.com>
2020-04-08 19:57:49 -0400

    Updating charsheet to allow character name in the message

--- Charsheet/1.2.2/charsheet.js
+++ Charsheet/1.2.2/charsheet.js
@@ -15,7 +15,7 @@ on("chat:message", function(msg) {
 
     if (msg.content.indexOf('!charsheet') != -1) {
         // replace the !charsheet command with nothing, leaving just the character name behind
-        var charName = msg.content;
+        charName = msg.content;
         charName.replace('!charsheet ', '');
         Charsheet.Generate(msg);
     }
